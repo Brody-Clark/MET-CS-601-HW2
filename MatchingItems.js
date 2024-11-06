76c94d344ae5a4115ecb1caf30dcc17128ae6a76
Brody Clark <brodyclark97@gmail.com>
2024-11-05 19:01:11 -0500

    adding homework files

---
+++ MatchingItems.js
@@ -0,0 +1,75 @@
+const itemContainer = document.getElementById("draggableItems");
+const fruitDropZone = document.getElementById('fruitDropZone');
+const vegDropZone = document.getElementById('vegDropZone');
+
+// Add event listeners for the fruit drop zone
+fruitDropZone.addEventListener('dragover', handleDragOver);
+fruitDropZone.addEventListener('dragleave', handleDragLeaveOrDrop);
+fruitDropZone.addEventListener('drop', (event) => {
+  handleDragLeaveOrDrop(event); // Remove visual feedback
+  const itemType = event.dataTransfer.getData('type');
+
+  if (itemType === 'fruit') {
+    const itemId = event.dataTransfer.getData('text/plain');
+    const fruit = document.getElementById(itemId);
+    fruitDropZone.appendChild(fruit); // Move the fruit to its drop zone
+  }
+});
+
+// Add event listeners for the vegetable drop zone
+vegDropZone.addEventListener('dragover', handleDragOver);
+vegDropZone.addEventListener('dragleave', handleDragLeaveOrDrop);
+vegDropZone.addEventListener('drop', (event) => {
+  handleDragLeaveOrDrop(event); // Remove visual feedback
+  const itemType = event.dataTransfer.getData('type');
+
+  if (itemType === 'vegetable') {
+    const itemId = event.dataTransfer.getData('text/plain');
+    const veg = document.getElementById(itemId);
+    vegDropZone.appendChild(veg); // Move the vegetable to its drop zone
+  }
+});
+
+
+function fetchItems(){
+    fetch("http://localhost:3000/data")//TODO: what to put here
+    .then((response) => response.json())
+    .then((body) => loadItems(body));
+}
+
+// Loop through json reponse and add new created elements to HTML
+function loadItems(items){
+    for(let i = 0; i < body.lenght; i++){
+        let obj = body[i];
+        let newItem = document.createElement("li");
+
+        newItem.setAttribute('category', obj.category);
+        newItem.setAttribute('draggable', 'true');
+        newItem.setAttribute('class', 'draggable-item');
+        newItem.setAttribute('id', obj.id);
+        newItem.textContent = obj.name;
+
+        itemContainer.appendChild(newItem);
+
+        newItem.addEventListener('dragstart', (event) => {
+            event.dataTransfer.setData('text/plain', event.target.id);
+            event.dataTransfer.setData('type', obj.category);
+            event.target.classList.add('dragging');
+        });
+        
+        newItem.addEventListener('dragend', (event) => {
+            event.target.classList.remove('dragging');
+        });
+    }
+}
+
+// Function to handle 'dragover' event for both drop zones
+function handleDragOver(event) {
+  event.preventDefault(); // Allow dropping
+  event.currentTarget.classList.add('drag-over');
+}
+
+// Function to handle 'dragleave' and 'drop' events for both drop zones
+function handleDragLeaveOrDrop(event) {
+  event.currentTarget.classList.remove('drag-over');
+}
